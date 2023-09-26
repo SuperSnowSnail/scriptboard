b0ae46db17d51e27cc98ea0ae32a1f6060fa2568
SnowSnail <f2961@yandex.ru>
2023-09-26 22:01:40 +0300

    Add running route

---
+++ app/api/scripts/running/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import prisma from "@/utils/initPrisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized!" }, { status: 401 });
+  }
+
+  const scripts = await prisma.runningScript.findMany({
+    where: {
+      launcherId: session.user.id,
+    },
+    select: {
+      id: true,
+      updatedAt: true,
+      name: true,
+      tier: true,
+    },
+  });
+
+  return NextResponse.json({ scripts }, { status: 200 });
+}
+
+export const dynamic = "force-dynamic";
