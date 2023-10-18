ff6c7fa109504a44d78331c5a7f88b1d5b085f6a
SnowSnail <f2961@yandex.ru>
2023-10-18 17:46:52 +0300

    Add archive form content

---
+++ components/Forms/Archive/Content.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Terminal from "@/components/Terminal";
+
+import { useArchiveFormContext } from "@/context/ArchiveFormContext";
+
+const Content = () => {
+  const { lines, mounted } = useArchiveFormContext();
+
+  if (!mounted) {
+    return (
+      <div className="hidden h-full w-full animate-pulse rounded-lg border border-gray-300 bg-white p-4 text-gray-900 dark:border-gray-600 dark:bg-gray-800 dark:text-white lg:order-3 lg:block lg:max-w-[calc(100vw_-_39rem)] lg:flex-1 lg:overflow-auto xl:max-w-[calc(100vw_-_43rem)]"></div>
+    );
+  }
+
+  return (
+    <div className="h-96 w-full max-w-[calc(100vw_-_2rem)] rounded-lg border border-gray-300 bg-white p-0 text-gray-900 dark:border-gray-600 dark:bg-gray-800 dark:text-white sm:max-w-[calc(100vw_-_18rem)] lg:order-3 lg:h-full lg:max-w-[calc(100vw_-_39rem)] lg:flex-1 lg:overflow-auto xl:max-w-[calc(100vw_-_43rem)]">
+      <Terminal lines={lines} />
+    </div>
+  );
+};
+
+export default Content;
